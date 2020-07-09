eb18c0c2b56c79db3952476702b70f44a40b4505
VladBoisa <60569670+vladboisa@users.noreply.github.com>
2020-07-10 01:11:51 +0300

    feat: add mobile layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,41 @@
 import '../css/style.css';
 import '../css/style.scss';
-import {
-  moduleOne
-} from './moduleOne';
 
-const helloArr = require('./moduleOne.js');
+const MENU = document.getElementById('menu');
+const body = document.querySelector('body');
+/* Hambgurger */
 
-
-
-class TestClass {
-  constructor() {
-    const msg = "Using ES2015+ syntax";
-    console.log(msg);
+const hambgurger = document.querySelector("#header > span:nth-child(3)");
+const headernav = document.getElementById('nav');
+const sect = document.querySelector("#nav");
+const links = document.querySelectorAll('#menu>li>a');
+hambgurger.addEventListener('click', () => {
+    if (headernav.style.left === "-100%" || headernav.style.left === "") {
+        hambgurger.classList.add("hamb_opened");
+        headernav.style.left = "0%";
+        headernav.style.top = "25%"
+        body.style.overflow = "hidden";
+        headernav.classList.add("shadow");
+    } else {
+        hambgurger.classList.remove("hamb_opened");
+        headernav.style.left = "-100%";
+        body.style.overflow = "visible";
+        headernav.classList.remove("shadow");
+    }
+});
+function closeBurg() {
+  if (hambgurger.classList.contains('hamb_opened')) {
+      sect.forEach((el) => {
+          links.forEach((a) => {
+              if (el.getAttribute('id') === a.getAttribute('href').substring(1)) {
+                  hambgurger.classList.remove("hamb_opened");
+                  headernav.style.left = "-100%";
+                  logo.style.paddingLeft = "148px";
+                  body.style.overflow = "visible";
+                  headernav.classList.remove("shadow");
+              }
+          });
+      });
   }
-}
-
-const test = new TestClass();
-
-
-// Пример массива
-console.log(helloArr);
-
-/* пример подключения модуля*/
-let mod = moduleOne(2, 3);
-
-console.log(mod);
+};
+MENU.addEventListener('click', closeBurg);
